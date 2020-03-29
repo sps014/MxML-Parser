67e6e47f15c5c48ff83fadac594483fecd26df7e
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-29 17:11:37 +0530

    Update Program.cs

--- MxML Parser/Program.cs
+++ MxML Parser/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using Helpers;
+using System.Xml.Schema;
 
 namespace MxML.Parser
 {
@@ -13,13 +16,39 @@ namespace MxML.Parser
             Console.ForegroundColor = ConsoleColor.White;
 
         }
-        //public static MxMLParsedData[] ParseData(string[] files)
-        //{
-        //    foreach(string file in files)
-        //    {
+        public static MxMLParsedData[] ParseData(string[] files)
+        {
+            List<MxMLParsedData> parseResult = new List<MxMLParsedData>();   
+            foreach (string file in files)
+            {
+                parseResult.Add(ParseSingleMxML(file));
+            }
+            return parseResult.ToArray();
+        }
+        private static MxMLParsedData ParseSingleMxML(string path)
+        {
+            MxMLParsedData result = new MxMLParsedData();
+            StreamReader reader = new StreamReader(path);
 
-        //    }
-        //}
+            //get version info
+            var versionInfo = GetXMLInfo(reader.ReadLine());
+
+            result.Version = versionInfo.Item1;
+            result.Encoding = versionInfo.Item2;
+
+            reader.Close();
+            //For the first line get the Version
+            return result;
+        }
+        /// <summary>
+        /// Parsing XML Here
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private static (string,string) GetXMLInfo(string line)
+        {
+
+        }
 
     }
 }
