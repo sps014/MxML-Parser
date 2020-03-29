bf29989f5ed408cb1e9ea10ed6b5065006622986
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-29 17:02:35 +0530

    data structure

--- MxML Parser/MxMLParsedData.cs
+++ MxML Parser/MxMLParsedData.cs
@@ -1,7 +1,16 @@
-﻿namespace MxML.Parser
+﻿using System.Collections.Generic;
+
+namespace MxML.Parser
 {
     public class MxMLParsedData
     {
-        
+        public string Version;
+        public string Encoding;
+        public ChildNode Child;
+    }
+    public class ChildNode
+    {
+        public ChildNode[] Children;
+        public Dictionary<string, string> Parameters;
     }
 }
