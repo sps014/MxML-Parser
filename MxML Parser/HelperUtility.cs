8676193d37181b1b4b3a35a453424025b24c110d
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-29 16:49:03 +0530

    folder read

--- MxML Parser/HelperUtility.cs
+++ MxML Parser/HelperUtility.cs
@@ -10,18 +10,32 @@ namespace Helpers
         public static string[] GetAllFilesOfExtension(string path,string extension=".mxml")
         {
             List<string> files = new List<string>();
-            DirectoryInfo folders = new DirectoryInfo(path);
-            var filesInfo = folders.GetFiles();
-            foreach(FileInfo info in filesInfo)
-            {
-                if(info.Extension==extension)
-                {
-                    files.Add(info.FullName);
-                }
-            }
+
+            RecursiveFolders(path, extension, ref files);
 
 
             return files.ToArray();
+        }
+
+        static void RecursiveFolders(string path,string extn,ref List<string> res)
+        {
+            DirectoryInfo folders = new DirectoryInfo(path);
+            var filesInfo = folders.GetFiles();
+            foreach (FileInfo info in filesInfo)
+            {
+                if (info.Extension == extn)
+                {
+                    Console.WriteLine(info.FullName);
+                    res.Add(info.FullName);
+
+                }
+
+            }
+
+            foreach (var folder in folders.GetDirectories())
+            {
+                RecursiveFolders(folder.FullName, extn, ref res);
+            }
         }
     }
 }
--- MxML Parser/Program.cs
+++ MxML Parser/Program.cs
@@ -7,7 +7,11 @@ namespace MxML.Parser
     {
         public static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
             HelperUtility.GetAllFilesOfExtension(@"C:\Users\shive\Desktop\loginscreen\flex");
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
         //public static MxMLParsedData[] ParseData(string[] files)
         //{
