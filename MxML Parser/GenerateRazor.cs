730d68e39486bd4162808d562946fcb1eb75e1e3
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-30 19:47:32 +0530

    hmm done razor

--- MxML Parser/GenerateRazor.cs
+++ MxML Parser/GenerateRazor.cs
@@ -8,27 +8,55 @@ namespace MxML.Parser
 {
     public static class GenerateRazor
     {
-        public static string GetRazorString(string path)
+        public static MxMLParsedData GetRazorString(string path)
         {
-            StreamReader reader = new StreamReader(path);
-            _=reader.ReadLine();
-            string xmlBody = reader.ReadToEnd();
-            xmlBody = RemoveCDATA(xmlBody);
-            xmlBody=xmlBody.Replace(':', '.');
-            reader.Close();
-            RemoveCDATA(xmlBody);
-            return xmlBody;
+            MxMLParsedData mxMLParsed = new MxMLParsedData();
+
+            var xml = ReadFile(path);
+
+            mxMLParsed.ActionCode = ParseCDATA(xml);
+            mxMLParsed.RazorCode = ParseTags(xml, mxMLParsed.ActionCode);
+            mxMLParsed.Path = path;
+
+            WriteFile(mxMLParsed.RazorCode);
+
+            return mxMLParsed;
         }
-        private static string RemoveCDATA(string str)
+
+        private static string ParseCDATA(string str)
         {
             var matches=Regex.Match(str, @"(<mx:Script>(.|\n)*?<\/mx:Script>)",
                                 RegexOptions.Multiline|RegexOptions.IgnoreCase);
 
             if (matches.Groups.Count >= 1)
-                if(matches.Groups[0].Value.Length>=1)
-                str=str.Replace(matches.Groups[0].Value, " ");
+                if (matches.Groups[0].Value.Length >= 1)
+                    return matches.Groups[0].Value;
+
+            return null;
+        }
+        private static string ParseTags(string str,string cdata)
+        {
+            if (cdata != null)
+                if (cdata.Length > 0)
+                    return str.Replace(cdata, "\n");
 
             return str;
+
+
+        }
+        private static string ReadFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+            _ = reader.ReadLine();
+            string xmlBody = reader.ReadToEnd();
+            reader.Close();
+            return xmlBody;
+        }
+        private static void WriteFile(string str)
+        {
+            StreamWriter sw = new StreamWriter("text.txt");
+            sw.Write(str);
+            sw.Close();
         }
     }
 }
--- MxML Parser/MxMLParsedData.cs
+++ MxML Parser/MxMLParsedData.cs
@@ -4,15 +4,10 @@ namespace MxML.Parser
 {
     public class MxMLParsedData
     {
-        public string Version { get; set; }
-        public string Encoding { get; set; }
-        public ChildNode Child { get; set; }
+       public string ActionCode { get; set; }
+       public string RazorCode { get; set; }
         public string Path { get; set; }
+
     }
-    public class ChildNode
-    {
-        public ChildNode[] Children { get; set; }
-        public Dictionary<string, string> Parameters { get; set; }
-        public string Name { get; set; }
-    }
+
 }
