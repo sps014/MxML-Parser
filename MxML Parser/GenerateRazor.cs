0eac10a1f4b8de4ce0f897e9efe501b5a4f3549f
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-03-30 19:22:11 +0530

    found an easy way

--- MxML Parser/GenerateRazor.cs
+++ MxML Parser/GenerateRazor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MxML.Parser
 {
@@ -12,9 +13,22 @@ namespace MxML.Parser
             StreamReader reader = new StreamReader(path);
             _=reader.ReadLine();
             string xmlBody = reader.ReadToEnd();
+            xmlBody = RemoveCDATA(xmlBody);
             xmlBody=xmlBody.Replace(':', '.');
             reader.Close();
+            RemoveCDATA(xmlBody);
             return xmlBody;
+        }
+        private static string RemoveCDATA(string str)
+        {
+            var matches=Regex.Match(str, @"(<mx:Script>(.|\n)*?<\/mx:Script>)",
+                                RegexOptions.Multiline|RegexOptions.IgnoreCase);
+
+            if (matches.Groups.Count >= 1)
+                if(matches.Groups[0].Value.Length>=1)
+                str=str.Replace(matches.Groups[0].Value, " ");
+
+            return str;
         }
     }
 }
--- MxML Parser/Parser.cs
+++ MxML Parser/Parser.cs
@@ -6,7 +6,6 @@ using System.Xml;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace MxML.Parser
 {
@@ -15,7 +14,6 @@ namespace MxML.Parser
         public static void Main(string[] args)
         {
             ParseData(@"C:\Users\shive\Desktop\loginscreen\flex");
-
         }
         public static MxMLParsedData[] ParseData(string path)
         {
@@ -26,12 +24,10 @@ namespace MxML.Parser
             var files=HelperUtility.GetAllFilesOfExtension(path, ".mxml");
             foreach (string file in files)
             {
+                GenerateRazor.GetRazorString(file);
+
                 HelperUtility.LogStatus($"started parsing {file}");
-                var pResult = ParseSingleMxML(file);
-                if (pResult != null)
-                    parseResult.Add(pResult);
-                else
-                    parseSuccessStatus = false;
+               
             }
 
             if (parseSuccessStatus)
@@ -42,191 +38,7 @@ namespace MxML.Parser
 
             return parseResult.ToArray();
         }
-        private static MxMLParsedData ParseSingleMxML(string path)
-        {
-            MxMLParsedData result = ParseChildern(path);
 
-            result.Path = path;
-
-            return result;
-        }
-
-
-        private static MxMLParsedData ParseChildern(string path)
-        {
-            XmlDocument document = new XmlDocument();
-            JObject data = null;
-            try
-            {
-                document.Load(path);
-
-                string json = JsonConvert.SerializeXmlNode(document);
-                data = JObject.Parse(json);
-            }
-            catch(Exception e)
-            {
-                HelperUtility.LogError(e.Message);
-            }
-            return JsonDataToMxMLParsed(data);
-        }
-        private static MxMLParsedData JsonDataToMxMLParsed(JObject data)
-        {
-            MxMLParsedData pObject = new MxMLParsedData();
-
-            var headResult=ParseHeader(data, ref pObject);
-            if (!headResult)
-                return null;
-
-            var childernResult = ParseChildern(data,ref pObject);
-            if (!childernResult)
-                return null;
-
-            return pObject;
-        }
-        private static bool ParseHeader(JObject data,ref MxMLParsedData pObject)
-        {
-            try
-            {
-                var version = ((JObject)data["?xml"])["@version"].Value<string>();
-                var encoding = ((JObject)data["?xml"])["@encoding"].Value<string>();
-                if (version == null || encoding == null)
-                    throw new Exception();
-
-                pObject.Encoding = encoding;
-                pObject.Version = version;
-                return true;
-            }
-            catch (Exception)
-            {
-                HelperUtility.LogError("Can't parse version and encoding ");
-                return false;
-            }
-        }
-        private static bool ParseChildern(JObject data,ref MxMLParsedData pObject)
-        {
-            foreach(var d in data)
-            {
-                if (d.Key == "?xml")
-                    continue;
-
-                pObject.Child=SolveRootNode((JObject)d.Value);
-                if (pObject.Child != null)
-                    pObject.Child.Name = d.Key;
-                else
-                    HelperUtility.LogError("Can't parse the Root Element");
-
-                break;
-            }
-            return true;
-        }
-        private static ChildNode SolveRootNode(JObject data)
-        {
-            ChildNode node = new ChildNode();
-            Dictionary<string, string> Parameters = new Dictionary<string, string>();
-            List<ChildNode> children = new List<ChildNode>();
-
-            foreach(var n in data)
-            {
-                if (isParameter(n.Key))
-                    Parameters.Add(RemoveAtRateSymbol(n.Key), n.Value.Value<string>());
-                else
-                {
-                    if(n.Key=="mx:script")
-                    {
-                        continue;
-                    }
-                    try
-                    { 
-                        var obj = (JObject)n.Value;
-                        var cn = SolveChildNode(obj, n.Key, ref node);
-                        if (cn != null)
-                            children.Add(cn);
-                    }
-                    catch(Exception)
-                    {
-                        var subList = (JArray)n.Value;
-                        var cn=SolveChildNode(subList, n.Key,ref node);
-                        if (cn != null)
-                            children.Add(cn);
-                    }
-
-                    //if (child != null)
-                    //    children.Add(child);
-                }
-            }
-            node.Parameters = Parameters;
-            node.Children = children.ToArray();
-            return node;
-        }
-        private static ChildNode SolveChildNode(JArray data,string name,ref ChildNode parent)
-        {
-            ChildNode node = new ChildNode();
-            //Dictionary<string, string> Parameters = new Dictionary<string, string>();
-            //List<ChildNode> children = new List<ChildNode>();
-
-            //foreach (var n in data)
-            //{
-            //    if (isParameter(n.))
-            //        Parameters.Add(RemoveAtRateSymbol(n.Key), n.Value.Value<string>());
-            //    else
-            //    {
-            //        var child = SolveChildNode((JObject)n.Value, n.Key, ref node);
-            //        if (child != null)
-            //            children.Add(child);
-            //    }
-            //}
-            //node.Parameters = Parameters;
-            //node.Children = children.ToArray();
-
-            return node;
-        }
-        private static ChildNode SolveChildNode(JObject data, string name, ref ChildNode parent)
-        {
-            ChildNode node = new ChildNode();
-            Dictionary<string, string> Parameters = new Dictionary<string, string>();
-            List<ChildNode> children = new List<ChildNode>();
-
-            foreach (var n in data)
-            {
-                if (isParameter(n.Key))
-                    Parameters.Add(RemoveAtRateSymbol(n.Key), n.Value.Value<string>());
-                else
-                {
-                    try
-                    {
-                        var obj = (JObject)n.Value;
-                        var cn = SolveChildNode(obj, n.Key, ref node);
-                        if (cn != null)
-                            children.Add(cn);
-                    }
-                    catch (Exception)
-                    {
-                        var subList = (JArray)n.Value;
-                        var cn = SolveChildNode(subList, n.Key, ref node);
-                        if (cn != null)
-                            children.Add(cn);
-                    }
-
-                }
-            }
-            node.Parameters = Parameters;
-            node.Children = children.ToArray();
-            return node;
-        }
-        private static bool isParameter(string str)
-        {
-            if (str[0] == '@')
-                return true;
-            else
-                return false;
-        }
-        private static string RemoveAtRateSymbol(string s)
-        {
-            if (s[0] == '@')
-                return s.Remove(0,1);
-
-            return s;
-        }
     }
 }
         
