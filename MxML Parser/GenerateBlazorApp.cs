7bdd6b7a2bd3d3400993ed519ca107a0660f22eb
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-01 16:31:25 +0530

    push

---
+++ MxML Parser/GenerateBlazorApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxML.Parser
+{
+    public static class GenerateBlazorApp
+    {
+        static bool isDotNetInstalled()
+        {
+            return false;
+        }
+    }
+}
