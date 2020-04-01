cf9e8a28d556411473c9e36f8c255cafa6c6c3bd
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-02 00:22:22 +0530

    C# convertor

---
+++ MxML Parser/SharpifyJava.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxML.Parser
+{
+    public static class SharpifyJava
+    {
+        public static string GetSharpified(string path)
+        {
+            return null;
+        }
+    }
+}
