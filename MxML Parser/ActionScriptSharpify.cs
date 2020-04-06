05b314c061db537a19b1495131e47bfcbf654a1d
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-06 20:57:17 +0530

    cdata

--- MxML Parser/ActionScriptSharpify.cs
+++ MxML Parser/ActionScriptSharpify.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MxML.Parser
 {
     public static class ActionScriptSharpify
     {
-        public string ParseCsharp(string actionCode)
+        public static string ParseCsharp(string actionCode)
         {
+            var Data = CleanCDATA(actionCode);
 
+
+            return Data;
+        }
+        private static string CleanCDATA(string str)
+        {
+            var match = Regex.Match(str,@"\<\!\[CDATA\[\s*((.|\n)*?)\]\s*\]\>");
+            if(match.Groups.Count>=2)
+            {
+                if(match.Groups[1].Value.Length>0)
+                {
+                    return match.Groups[1].Value;
+                }
+            }
+            return str;
         }
     }
 }
